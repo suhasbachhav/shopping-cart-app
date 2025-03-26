3be62656b85c58060eae4a0fc892ba55273b2fb9
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-26 22:16:01 +0530

    refactor code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './components/Shop/Products';
 import { useEffect } from 'react';
 import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,43 +17,12 @@ function App() {
   
 
   useEffect(()=>{
-    const sendCartData = async (cart) => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      }));
-
-      const response = await fetch('https://shopping-cart-fff20-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-
-      if(!response.ok){
-        throw new Error('Sending cart data failed');
-      }
-      const responseData = await response.json();
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success...',
-        message: 'Sent cart data successfully!',
-      }));
-    };
 
     if(isInitial){
       isInitial = false;
       return;
     }
-
-    sendCartData(cart).catch((error) => {
-      console.log(error);
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error...',
-        message: 'Sending cart data failed!',
-      }));
-    });
+    dispatch(sendCartData(cart));
   },[cart, dispatch]);
 
   return (
